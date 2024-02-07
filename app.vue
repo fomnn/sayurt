d106ba7652996d3a248099550b2c24ee068631be
Fathur Rahman N <95164774+omanfathurrahman@users.noreply.github.com>
2024-02-07 20:44:10 +0700

    feat: add page transition
    add page transition using vue transition

--- app.vue
+++ app.vue
@@ -10,4 +10,14 @@
 html {
   font-family: "DM Sans", sans-serif;
 }
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+  @apply translate-y-[5rem]
+}
 </style>
